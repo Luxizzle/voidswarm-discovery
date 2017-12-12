1c3496ae88d773b78b199704e257e48de8c6d876
Jelmer <luxizzle@gmail.com>
2017-12-12 09:58:19 +0100

    add ready

--- test.js
+++ test.js
@@ -2,5 +2,6 @@ var Discovery = require('./index')
 
 var d = new Discovery()
 
+d.on('ready', () => console.log('ready'))
 d.on('discovered', (address) => console.log('discovered', address))
 d.on('timeout', (address) => console.log('timeout', address))
