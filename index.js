6289fbea7aee9d96d01fd39a561347ccadd3764d
Jelmer <luxizzle@gmail.com>
2018-06-29 21:39:59 +0200

    code style

--- index.js
+++ index.js
@@ -1,74 +1,95 @@
-const EventEmitter = require('events').EventEmitter
-const dgram = require('dgram')
-const localIp = require('my-local-ip')
+const EventEmitter = require("events").EventEmitter;
+const dgram = require("dgram");
+const localIp = require("my-local-ip");
 
 const defaultOptions = {
-  name: 'voidswarm',
-  version: require('./package.json').version,
+  name: "voidswarm",
+  version: require("./package.json").version,
   port: 59544,
-  address: '255.255.255.255',
+  address: "255.255.255.255",
   blacklist: [],
   interval: 250,
   noAnnounce: false
-}
+};
 
 module.exports = class VoidswarmDiscovery extends EventEmitter {
   constructor(options = {}) {
-    super()
+    super();
 
-    this.options = Object.assign({}, defaultOptions, options)
-    this.options.blacklist.push(localIp())
+    this.options = Object.assign({}, defaultOptions, options);
+    this.options.blacklist.push(localIp());
 
-    this.identifier = [this.options.name, this.options.version].join('\0')
+    this.identifier = [this.options.name, this.options.version].join("\0");
 
-    this.nodes = new Set()
-    this.nodesTimeout = new Map()
+    this.nodes = new Set();
+    this.nodesTimeout = new Map();
 
-    this.socket = dgram.createSocket('udp4')
+    this.socket = dgram.createSocket("udp4");
 
-    this.socket.on('message', this.onMessage.bind(this))
+    this.socket.on("message", this.onMessage.bind(this));
 
     this.socket.bind(this.options.port, () => {
-      this.socket.setBroadcast(true)
-      this.emit('ready')
-    })
+      this.socket.setBroadcast(true);
+      this.emit("ready");
+    });
 
     if (this.options.noAnnounce === false) {
-      this.announceInterval = setInterval(this.announce.bind(this), this.options.interval)
+      this.announceInterval = setInterval(
+        this.announce.bind(this),
+        this.options.interval
+      );
     }
   }
 
   onMessage(message, remote) {
-    const address = remote.address
+    const address = remote.address;
     if (this.options.blacklist.indexOf(address) !== -1) return;
 
     if (message.toString() === this.identifier) {
       if (this.nodes.has(address)) {
-        clearTimeout(this.nodesTimeout.get(address))
-        this.nodesTimeout.set(address, setTimeout(this.timeout.bind(this), this.options.interval*2, address))
+        clearTimeout(this.nodesTimeout.get(address));
+        this.nodesTimeout.set(
+          address,
+          setTimeout(
+            this.timeout.bind(this),
+            this.options.interval * 2,
+            address
+          )
+        );
       } else {
-        this.nodes.add(address)
-        this.nodesTimeout.set(address, setTimeout(this.timeout.bind(this), this.options.interval*2, address))
-        this.emit('discovered', address)        
+        this.nodes.add(address);
+        this.nodesTimeout.set(
+          address,
+          setTimeout(
+            this.timeout.bind(this),
+            this.options.interval * 2,
+            address
+          )
+        );
+        this.emit("discovered", address);
       }
     }
   }
 
   announce() {
-    const message = new Buffer(this.identifier)
+    const message = new Buffer(this.identifier);
 
-    this.socket.send(message, 0, message.length, this.options.port, this.options.address)
+    this.socket.send(
+      message,
+      0,
+      message.length,
+      this.options.port,
+      this.options.address
+    );
   }
 
   timeout(address) {
     if (this.nodes.has(address)) {
-      this.nodes.delete(address)
-      this.nodesTimeout.delete(address)
-      this.emit('timeout', address)
+      this.nodes.delete(address);
+      this.nodesTimeout.delete(address);
+      this.emit("timeout", address);
     }
   }
 
-  close() {
-
-  }
-}
+  close() {}
+};
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-var Discovery = require('./index')
+var Discovery = require("./index");
 
-var d = new Discovery()
+var d = new Discovery();
 
-d.on('ready', () => console.log('ready'))
-d.on('discovered', (address) => console.log('discovered', address))
-d.on('timeout', (address) => console.log('timeout', address))
+d.on("ready", () => console.log("ready"));
+d.on("discovered", address => console.log("discovered", address));
+d.on("timeout", address => console.log("timeout", address));
